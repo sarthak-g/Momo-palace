43b27e6fd2f0fe075c41aa9928b7f1f1f702b77a
Sarthak gupta <sarthakgupta041@gmail.com>
2020-09-20 08:38:05 +0530

    project layout

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const path = require('path')
+const ejs = require('ejs');
+const expressLayout = require('express-ejs-layouts');
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+app.use(expressLayout)
+app.set('views', path.join(__dirname, '/resources/views'))
+app.set('view engine', 'ejs')
+
+app.listen(3000, () => {
+    console.log(`listening on port ${PORT}`)
+})
