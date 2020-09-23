b8e86a02c34c3ca2a26a1b6c12d0febf31a12e59
Sarthak gupta <sarthakgupta041@gmail.com>
2020-09-23 14:41:16 +0530

    layout of cart page

--- server.js
+++ server.js
@@ -7,13 +7,25 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static('public'))
 
+app.use(expressLayout)
+app.set('views', path.join(__dirname, '/resources/views'))
+app.set('view engine', 'ejs')
+
 app.get('/', (req, res) => {
     res.render('home')
 })
 
-app.use(expressLayout)
-app.set('views', path.join(__dirname, '/resources/views'))
-app.set('view engine', 'ejs')
+app.get('/cart', (req, res) => {
+    res.render('customers/cart')
+})
+
+app.get('/login', (req, res) => {
+    res.render('auth/login')
+})
+
+app.get('/register', (req, res) => {
+    res.render('auth/register')
+})
 
 app.listen(3000, () => {
     console.log(`listening on port ${PORT}`)
